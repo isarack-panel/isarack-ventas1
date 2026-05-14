c6143931ecb098fd7f4b3b1c17b07d3ed2860dcf
isarack-panel <licosuna88@gmail.com>
2026-05-14 13:16:31 -0700

    Update tc.js

--- api/tc.js
+++ api/tc.js
@@ -2,12 +2,13 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const TOKEN = 'dad63931f69f3192e813f16291b859d66bab26ed2245ee3bf375a9def048cd2c';
   const today = new Date();
+  const ayer = new Date(today); ayer.setDate(ayer.getDate() - 1);
   const pad = n => String(n).padStart(2, '0');
-  const fechaHoy = `${today.getFullYear()}-${pad(today.getMonth()+1)}-${pad(today.getDate())}`;
+  const fechaAyer = `${ayer.getFullYear()}-${pad(ayer.getMonth()+1)}-${pad(ayer.getDate())}`;
   const hace10 = new Date(today); hace10.setDate(hace10.getDate() - 10);
   const fechaDesde = `${hace10.getFullYear()}-${pad(hace10.getMonth()+1)}-${pad(hace10.getDate())}`;
   try {
-    const url = `https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF60653/datos/${fechaDesde}/${fechaHoy}?token=${TOKEN}`;
+    const url = `https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/${fechaDesde}/${fechaAyer}?token=${TOKEN}`;
     const r = await fetch(url);
     const d = await r.json();
     const datos = d?.bmx?.series?.[0]?.datos?.filter(x => x.dato !== 'N/E');
