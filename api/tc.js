6cbd193f4374bab25ac5210d718217dcf27e63ac
isarack-panel <licosuna88@gmail.com>
2026-05-14 13:08:34 -0700

    Update tc.js

--- api/tc.js
+++ api/tc.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   const hace10 = new Date(today); hace10.setDate(hace10.getDate() - 10);
   const fechaDesde = `${hace10.getFullYear()}-${pad(hace10.getMonth()+1)}-${pad(hace10.getDate())}`;
   try {
-    const url = `https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/${fechaDesde}/${fechaHoy}?token=${TOKEN}`;
+    const url = `https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF60653/datos/${fechaDesde}/${fechaHoy}?token=${TOKEN}`;
     const r = await fetch(url);
     const d = await r.json();
     const datos = d?.bmx?.series?.[0]?.datos?.filter(x => x.dato !== 'N/E');
