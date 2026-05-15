4867a82e6c0e88e8e36c3afeabe0961fb84af8e6
isarack-panel <licosuna88@gmail.com>
2026-05-14 17:52:00 -0700

    Update tc.js

--- api/tc.js
+++ api/tc.js
@@ -4,11 +4,12 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const TOKEN = 'dad63931f69f3192e813f16291b859d66bab26ed2245ee3bf375a9def048cd2c';
   const today = new Date();
+  const ayer = new Date(today); ayer.setDate(ayer.getDate() - 1);
   const pad = n => String(n).padStart(2, '0');
-  const fechaHoy = `${today.getFullYear()}-${pad(today.getMonth()+1)}-${pad(today.getDate())}`;
+  const fechaAyer = `${ayer.getFullYear()}-${pad(ayer.getMonth()+1)}-${pad(ayer.getDate())}`;
   const hace10 = new Date(today); hace10.setDate(hace10.getDate() - 10);
   const fechaDesde = `${hace10.getFullYear()}-${pad(hace10.getMonth()+1)}-${pad(hace10.getDate())}`;
-  const path = `/SieAPIRest/service/v1/series/SF43718/datos/${fechaDesde}/${fechaHoy}?token=${TOKEN}`;
+  const path = `/SieAPIRest/service/v1/series/SF43718/datos/${fechaDesde}/${fechaAyer}?token=${TOKEN}`;
 
   return new Promise((resolve) => {
     https.get({ hostname: 'www.banxico.org.mx', path, headers: { 'Bmx-Token': TOKEN } }, (r) => {
