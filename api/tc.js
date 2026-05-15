c1c2e03becee76d9df35867d6f81fb25f35b9609
isarack-panel <licosuna88@gmail.com>
2026-05-14 17:54:48 -0700

    Update tc.js

--- api/tc.js
+++ api/tc.js
@@ -4,13 +4,13 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const TOKEN = 'dad63931f69f3192e813f16291b859d66bab26ed2245ee3bf375a9def048cd2c';
   const today = new Date();
-  const ayer = new Date(today); ayer.setDate(ayer.getDate() - 1);
+  const hace2 = new Date(today); hace2.setDate(hace2.getDate() - 2);
   const pad = n => String(n).padStart(2, '0');
-  const fechaAyer = `${ayer.getFullYear()}-${pad(ayer.getMonth()+1)}-${pad(ayer.getDate())}`;
-  const fechaAyerBmx = `${pad(ayer.getDate())}/${pad(ayer.getMonth()+1)}/${ayer.getFullYear()}`;
+  const fecha2 = `${hace2.getFullYear()}-${pad(hace2.getMonth()+1)}-${pad(hace2.getDate())}`;
+  const fecha2Bmx = `${pad(hace2.getDate())}/${pad(hace2.getMonth()+1)}/${hace2.getFullYear()}`;
   const hace10 = new Date(today); hace10.setDate(hace10.getDate() - 10);
   const fechaDesde = `${hace10.getFullYear()}-${pad(hace10.getMonth()+1)}-${pad(hace10.getDate())}`;
-  const path = `/SieAPIRest/service/v1/series/SF43718/datos/${fechaDesde}/${fechaAyer}?token=${TOKEN}`;
+  const path = `/SieAPIRest/service/v1/series/SF43718/datos/${fechaDesde}/${fecha2}?token=${TOKEN}`;
 
   return new Promise((resolve) => {
     https.get({ hostname: 'www.banxico.org.mx', path, headers: { 'Bmx-Token': TOKEN } }, (r) => {
@@ -21,8 +21,7 @@ module.exports = async function handler(req, res) {
           const d = JSON.parse(data);
           const datos = d?.bmx?.series?.[0]?.datos?.filter(x => x.dato !== 'N/E');
           if (datos && datos.length > 0) {
-            const exacto = datos.find(x => x.fecha === fechaAyerBmx);
-            const ultimo = exacto || datos[datos.length - 1];
+            const ultimo = datos[datos.length - 1];
             const val = parseFloat(ultimo.dato);
             if (!isNaN(val) && val > 5) {
               res.status(200).json({ val, fecha: ultimo.fecha });
